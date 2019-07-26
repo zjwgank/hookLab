58969201d1551b5026beb36c7baedd70e2b6e74a
zjwgank <zjwgank@appledeMacBook-Pro.local>
2019-07-26 15:43:22 +0800

    feat: test1

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 module.exports = {
     '*.js':(files) => {
-        return files.map(item => `eslint --fix ${item}`) 
+        return files.map(item => `prettier --write ${item}`) 
     },
     '*.sh':(files) => {
         console.log(fs.stat(files),'======')
